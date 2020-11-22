e4e4ae9ee2aa3f0650d8251226b117bbb27eb2e1
Marcos Vinicius <mvnpereira@gmail.com>
2020-11-22 13:58:49 -0300

    Trabalhando com Foreach e map

---
+++ maps-08/for-forin-forof/map.js
@@ -0,0 +1,23 @@
+const service = require('../service')
+
+async function main() {
+
+    try {
+        const results = await service.obterPessoas('a')
+        
+        // const names = []
+        // results.results.forEach(function(item){
+        //     names.push(item.name)
+        // })
+
+        const names = results.results.map((pessoa) => pessoa.name)
+        console.log('names', names)
+
+    } 
+    catch (error) 
+    {   
+        console.error('DEU RUIM', error)
+    }
+}
+
+main()
